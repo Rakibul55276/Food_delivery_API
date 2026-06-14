36ff348060fc2f8fa1a13038dceaf438da792dd9
Rakibul55276 <irakibul550@gmail.com>
2026-06-14 09:05:31 +0300

    Fix public storage link

--- docker/start.sh
+++ docker/start.sh
@@ -4,7 +4,9 @@ php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-php artisan storage:link || true
+rm -rf public/storage
+php artisan storage:link
+
 php artisan migrate --force
 
 apache2-foreground
